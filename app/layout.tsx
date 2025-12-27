5b638ebed44fbc57c4258e60ad011b4262e90739
Mora-net <rab210221@gmail.com>
2025-12-27 09:21:56 +0700

    updateHome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,32 @@
+
 "use client";
 
 import "./globals.css";
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
+  const hideLayout = pathname === "/checkout";
+
   return (
     <html lang="en">
       <body>
-        <Navbar />
-         <main className="min-h-screen">
+        {!hideLayout && <Navbar />}
+
+        <main className="min-h-screen">
           {children}
         </main>
-        <Footer />
+
+        {!hideLayout && <Footer />}
       </body>
     </html>
   );
 }
+
